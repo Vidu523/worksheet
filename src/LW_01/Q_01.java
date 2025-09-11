0cd563e6fe017fbdf7ae1f7099b196d0d7b35a59
Vidu523 <vidyadinanjalie.523@gmail.com>
2025-09-11 15:14:41 +0530

    Q1 finish

--- src/LW_01/Q_01.java
+++ src/LW_01/Q_01.java
@@ -1,14 +1,7 @@
 package LW_01;
-import java.util.Scanner;
+
 public class Q_01 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter an odd-length word:");
-        String word = scanner.nextLine();
-        int middleIndex = word.length()/2;
-        char letter = word.charAt(middleIndex);
-        System.out.println("The middle character is " + letter);
-        scanner.close();
 
     }
 }
